41fda717c7a45de95564840d02cf405f4543d104
Michael Wunderlich <mwunderl@amazon.com>
2019-04-18 09:09:31 -0700

    error processor

--- sample-apps/error-processor/deploy.sh
+++ sample-apps/error-processor/deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 aws cloudformation package --template-file error-processor.yaml --s3-bucket MY_BUCKET --output-template-file out.yaml
-aws cloudformation deploy --template-file out.yaml --stack-name lambda-error-processor --capabilities CAPABILITY_NAMED_IAM
+aws cloudformation deploy --template-file out.yaml --stack-name error-processor --capabilities CAPABILITY_NAMED_IAM
