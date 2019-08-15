9832568f66b5b740691c1dd67f932790f49fbbb0
Michael Wunderlich <mwunderl@amazon.com>
2019-08-14 22:12:29 -0700

    sample Node.js function

--- sample-apps/blank/function/index.js
+++ sample-apps/blank/function/index.js
@@ -1,22 +1,23 @@
-// Utils
-var zlib = require('zlib');
 // AWS X-Ray SDK
 var AWSXRay = require('aws-xray-sdk-core')
 // AWS SDK clients
+// NOTE: Function needs permission for each service in execution role
 var AWS = AWSXRay.captureAWS(require('aws-sdk'))
+// Create client outside of handler to reuse 
 var s3 = new AWS.S3()
-
-// environment variable
+// Read bucket name from environment variable
 const bucket = process.env.bucket
 
-// use S3 SDK
+// Use Amazon S3 client
 var putObject = function(data, filename, context){
+  // Create request for PutObject action
   var s3params = {
     Body: JSON.stringify(data, null, 2),
     Bucket: bucket,
     Key: filename
   };
-   s3.putObject(s3params, function(err, data) {
+  // Send request and log result
+  s3.putObject(s3params, function(err, data) {
      if (err) console.log(err, err.stack)
      else     console.log(data)
   });
@@ -24,6 +25,6 @@ var putObject = function(data, filename, context){
 
 exports.handler = function(event, context) {
     console.log("Event: " + JSON.stringify(event, null, 2))
-    // store event
+    // Wait one second and store event in S3
     setTimeout(function(){putObject(event, "test", context)}, 1000)
 };
