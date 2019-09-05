500357fdc87f14c93380d7374c10807cf619344f
Michael Wunderlich <mwunderl@amazon.com>
2019-09-05 16:16:03 -0700

    sample templates

--- sample-apps/blank/cleanup.sh
+++ sample-apps/blank/cleanup.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 set -eo pipefail
+OUTPUT_BUCKET=$(aws cloudformation describe-stack-resource --stack-name blank --logical-resource-id bucket --query 'StackResourceDetail.PhysicalResourceId' --output text)
+while true; do
+    read -p "Delete output bucket ($OUTPUT_BUCKET)?" response
+    case $response in
+        [Yy]* ) aws s3 rb --force s3://$OUTPUT_BUCKET; aws cloudformation delete-stack --stack-name blank; break;;
+        [Nn]* ) aws cloudformation delete-stack --stack-name blank --retain-resources bucket; exit;;
+        * ) echo "Response must start with y or n.";;
+    esac
+done
+echo "Deleted function stack"
 aws cloudformation delete-stack --stack-name blank
 if [ -f bucket-name.txt ]; then
     ARTIFACT_BUCKET=$(cat bucket-name.txt)
