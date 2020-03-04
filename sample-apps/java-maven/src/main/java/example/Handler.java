b236621483bc333a73103dd0ce68291f9b6bbcba
Michael Wunderlich <mwunderl@amazon.com>
2020-03-03 17:05:54 -0800

    basic java function

--- sample-apps/java-maven/src/main/java/example/Handler.java
+++ sample-apps/java-maven/src/main/java/example/Handler.java
@@ -3,17 +3,15 @@ package example;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.LambdaLogger;
-import com.amazonaws.services.lambda.runtime.events.SQSEvent;
-import com.amazonaws.services.lambda.runtime.events.SQSEvent.SQSMessage;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
 // Handler value: example.Handler
-public class Handler implements RequestHandler<SQSEvent, String>{
+public class Handler implements RequestHandler<Object, String>{
   Gson gson = new GsonBuilder().setPrettyPrinting().create();
   @Override
-  public String handleRequest(SQSEvent event, Context context)
+  public String handleRequest(Object event, Context context)
   {
     LambdaLogger logger = context.getLogger();
     String response = new String("SUCCESS");
--- sample-apps/java-maven/src/test/java/example/InvokeTest.java
+++ sample-apps/java-maven/src/test/java/example/InvokeTest.java
@@ -6,8 +6,6 @@ import org.junit.jupiter.api.Test;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
-import com.amazonaws.services.lambda.runtime.events.SQSEvent;
-import com.amazonaws.services.lambda.runtime.events.SQSEvent.SQSMessage;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -21,8 +19,7 @@ class InvokeTest {
   @Test
   void invokeTest() {
     logger.info("Invoke TEST");
-    SQSEvent event = new SQSEvent();
-    event.setRecords(new ArrayList<SQSMessage>());
+    Object event = new Object();
     Context context = new TestContext();
     String requestId = context.getAwsRequestId();
     Handler handler = new Handler();
