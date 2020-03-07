5444b812ea66c4687df8312bc7f3d697b7cd5baf
Michael Wunderlich <mwunderl@amazon.com>
2020-03-06 16:44:55 -0800

    stream handler

---
+++ sample-apps/java-basic/src/main/java/example/HandlerStream.java
@@ -0,0 +1,54 @@
+package example;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
+import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonSyntaxException;
+
+
+import java.util.HashMap;
+import java.io.InputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.io.BufferedWriter;
+import java.nio.charset.Charset;
+import java.lang.IllegalStateException;
+
+// Handler value: example.Handler
+public class HandlerStream implements RequestStreamHandler {
+  Gson gson = new GsonBuilder().setPrettyPrinting().create();
+  @Override
+  public void handleRequest(InputStream inputStream, OutputStream outputStream, Context context) throws IOException
+  {
+    LambdaLogger logger = context.getLogger();
+    BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, Charset.forName("US-ASCII")));
+    PrintWriter writer = new PrintWriter(new BufferedWriter(new OutputStreamWriter(outputStream, Charset.forName("US-ASCII"))));
+    try
+    {
+      HashMap event = gson.fromJson(reader, HashMap.class);
+      logger.log("STREAM TYPE: " + inputStream.getClass().toString());
+      logger.log("EVENT TYPE: " + event.getClass().toString());
+      writer.write(gson.toJson(event));
+      if (writer.checkError())
+      {
+        logger.log("WARNING: Writer encountered an error.");
+      }
+    }
+    catch (IllegalStateException | JsonSyntaxException exception)
+    {
+      logger.log(exception.toString());
+    }
+    finally
+    {
+      reader.close();
+      writer.close();
+    }
+  }
+}
