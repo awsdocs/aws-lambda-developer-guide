3680f1e333bfddbd3d86242bdd874f93d9e67d46
Michael Wunderlich <mwunderl@amazon.com>
2020-03-16 22:26:49 -0700

    working test event deserialization with mangled records key

--- sample-apps/blank-java/src/test/java/example/InvokeTest.java
+++ sample-apps/blank-java/src/test/java/example/InvokeTest.java
@@ -10,19 +10,31 @@ import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.SQSEvent;
 import com.amazonaws.services.lambda.runtime.events.SQSEvent.SQSMessage;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.List;
 import java.util.ArrayList;
 
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+ 
 class InvokeTest {
   private static final Logger logger = LoggerFactory.getLogger(InvokeTest.class);
-
+  Gson gson = new GsonBuilder().setPrettyPrinting().create();
   @Test
   void invokeTest() {
-    SQSEvent event = new SQSEvent();
-    event.setRecords(new ArrayList<SQSMessage>());
+
+    String path = "src/test/resources/event.json";
+    String eventString = loadJsonFile( path );
+    logger.info("EVENT STRING: " + eventString);
+    SQSEvent event = new Gson().fromJson(eventString, SQSEvent.class);
     Context context = new TestContext();
     String requestId = context.getAwsRequestId();
     Handler handler = new Handler();
@@ -30,4 +42,17 @@ class InvokeTest {
     assertTrue(result.contains("totalCodeSize"));
   }
 
+  private static String loadJsonFile(String path) 
+  {
+      StringBuilder stringBuilder = new StringBuilder();
+      try (Stream<String> stream = Files.lines( Paths.get(path), StandardCharsets.UTF_8)) 
+      {
+          stream.forEach(s -> stringBuilder.append(s));
+      }
+      catch (IOException e) 
+      {
+          e.printStackTrace();
+      }
+      return stringBuilder.toString();
+  }
 }
