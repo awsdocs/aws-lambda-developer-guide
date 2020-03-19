05b5a536843d81ecb2e2d35020a58c77dc083dc3
Michael Wunderlich <mwunderl@amazon.com>
2020-03-18 17:43:01 -0700

    division handler

---
+++ sample-apps/java-basic/src/main/java/example/HandlerDivide.java
@@ -0,0 +1,31 @@
+package example;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.util.List;
+import java.lang.RuntimeException;
+
+// Handler value: example.HandlerList
+public class HandlerDivide implements RequestHandler<List<Integer>, Integer>{
+  Gson gson = new GsonBuilder().setPrettyPrinting().create();
+  @Override
+  public Integer handleRequest(List<Integer> event, Context context)
+  {
+    LambdaLogger logger = context.getLogger();
+    // process event
+    if ( event.size() != 2 )
+    {
+      throw new RuntimeException("Input must contain 2 numbers.");
+    }
+    int numerator = event.get(0);
+    int denominator = event.get(1);
+    logger.log("EVENT: " + gson.toJson(event));
+    logger.log("EVENT TYPE: " + event.getClass().toString());
+    return numerator/denominator;
+  }
+}
