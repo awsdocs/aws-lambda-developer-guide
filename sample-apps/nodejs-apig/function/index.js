fa1368a9d56d1b10572a5f921a9b82dc8b89010a
Michael Wunderlich <mwunderl@amazon.com>
2020-03-20 22:47:41 -0700

    handle errors

--- sample-apps/nodejs-apig/function/index.js
+++ sample-apps/nodejs-apig/function/index.js
@@ -4,6 +4,18 @@ const lambda = new AWS.Lambda()
 
 // Handler
 exports.handler = async function(event, context) {
+  console.log('ENVIRONMENT VARIABLES: ' + serialize(process.env))
+  console.log('CONTEXT: ' + serialize(context))
+  console.log('EVENT: ' + serialize(event))
+  try {
+    let accountSettings = await getAccountSettings()
+    return formatResponse(serialize(accountSettings.AccountUsage))
+  } catch(error) {
+    return formatError(error)
+  }
+}
+
+var formatResponse = function(body){
   var response = {
     "statusCode": 200,
     "headers": {
@@ -13,16 +25,23 @@ exports.handler = async function(event, context) {
     "multiValueHeaders": { 
       "X-Custom-Header": ["My value", "My other value"],
     },
-    "body": ""
+    "body": body
   }
-  console.log('ENVIRONMENT VARIABLES: ' + serialize(process.env))
-  console.log('CONTEXT: ' + serialize(context))
-  console.log('EVENT: ' + serialize(event))
-  var accountSettings = await getAccountSettings()
-  response.body = serialize(accountSettings.AccountUsage)
   return response
 }
 
+var formatError = function(error){
+  var response = {
+    "statusCode": error.statusCode,
+    "headers": {
+      "Content-Type": "text/plain",
+      "x-amzn-ErrorType": error.code
+    },
+    "isBase64Encoded": false,
+    "body": error.code + ": " + error.message
+  }
+  return response
+}
 // Use SDK client
 var getAccountSettings = function(){
   return lambda.getAccountSettings().promise()
