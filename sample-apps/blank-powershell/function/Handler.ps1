ec0ffdb09e9d31c1c9bf551e6c21733acbb7f601
Michael Wunderlich <mwunderl@amazon.com>
2020-03-27 16:36:03 -0700

    PowerShell sample app

--- sample-apps/blank-powershell/function/Handler.ps1
+++ sample-apps/blank-powershell/function/Handler.ps1
@@ -1,4 +1,17 @@
 #Requires -Modules @{ModuleName='AWSPowerShell.NetCore';ModuleVersion='3.3.618.0'}
 $region = $env:AWS_REGION
+# Process event
+foreach ($message in $LambdaInput.Records)
+{
+    Write-Host $message.body
+}
+# Log environment details
+# Environment variables
 Write-Host 'Region:' $region
+# Event
+Write-Host (ConvertTo-Json -InputObject $LambdaInput -Compress -Depth 5)
+# Context
+Write-Host (ConvertTo-Json -InputObject $LambdaContext -Compress -Depth 5)
 Write-Host 'Function name:' $LambdaContext.FunctionName
+# Call Lambda API
+Get-LMAccountSetting | Select-Object -Property AccountUsage
