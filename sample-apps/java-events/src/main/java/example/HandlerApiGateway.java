8a6bc414925ce1ae330492c10705b1cd77574577
Jiahao <jiahao.chen@mail.mcgill.ca>
2021-06-15 19:22:00 -0700

    Remove unused import

--- sample-apps/java-events/src/main/java/example/HandlerApiGateway.java
+++ sample-apps/java-events/src/main/java/example/HandlerApiGateway.java
@@ -2,7 +2,6 @@ package example;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
-import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
 
