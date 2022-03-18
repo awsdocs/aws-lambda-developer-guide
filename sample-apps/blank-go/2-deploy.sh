785c872d0a24451d674102be74a36b80fff8e7cf
Scott Schlesier <scott@schlesier.ca>
2022-03-18 07:08:54 -0700

    GOARCH amd64
    To ensure the go sample is compiled for x86 when being run from ARM or other systems.

--- sample-apps/blank-go/2-deploy.sh
+++ sample-apps/blank-go/2-deploy.sh
@@ -2,7 +2,7 @@
 set -eo pipefail
 ARTIFACT_BUCKET=$(cat bucket-name.txt)
 cd function
-GOOS=linux go build main.go
+GOOS=linux GOARCH=amd64 go build main.go
 cd ../
 aws cloudformation package --template-file template.yml --s3-bucket $ARTIFACT_BUCKET --output-template-file out.yml
 aws cloudformation deploy --template-file out.yml --stack-name blank-go --capabilities CAPABILITY_NAMED_IAM
