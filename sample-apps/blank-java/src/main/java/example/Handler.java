89c31dd72e8d3e10d269f1ca84b7d082bf43365b
Alexander Yu <yualexan@amazon.com>
2023-02-09 15:14:36 -0800

    Address @msailes comments

--- sample-apps/blank-java/src/main/java/example/Handler.java
+++ sample-apps/blank-java/src/main/java/example/Handler.java
@@ -8,6 +8,7 @@ import java.util.Map;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import software.amazon.awssdk.core.SdkSystemSetting;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.lambda.LambdaClient;
 import software.amazon.awssdk.services.lambda.model.GetAccountSettingsResponse;
@@ -17,9 +18,8 @@ import software.amazon.awssdk.services.lambda.model.LambdaException;
 public class Handler implements RequestHandler<Map<String,String>, String> {
 
     private static final Logger logger = LoggerFactory.getLogger(Handler.class);
-    private static final Region region = Region.US_EAST_1;
-    private static final LambdaClient awsLambda = LambdaClient.builder()
-        .region(region)
+    private static final LambdaClient lambdaClient = LambdaClient.builder()
+        .region(Region.of(System.getenv(SdkSystemSetting.AWS_REGION.environmentVariable())))
         .build();
 
     @Override
@@ -30,14 +30,12 @@ public class Handler implements RequestHandler<Map<String,String>, String> {
         logger.info("EVENT: " + event);
 
         // process event
-        GetAccountSettingsResponse response;
+        GetAccountSettingsResponse response = null;
         try {
-            response = awsLambda.getAccountSettings();
-            System.out.println("Total code size for your account is " + response.accountLimit().totalCodeSize() + " bytes");
+            response = lambdaClient.getAccountSettings();
         } catch(LambdaException e) {
             System.err.println(e.getMessage());
-            System.exit(1);
         }
-        return "200 OK";
+        return response != null ? "Total code size for your account is " + response.accountLimit().totalCodeSize() + " bytes" : "Error";
     }
 }
--- sample-apps/blank-java/src/test/java/example/InvokeTest.java
+++ sample-apps/blank-java/src/test/java/example/InvokeTest.java
@@ -1,21 +1,12 @@
 package example;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
 
 import com.amazonaws.services.lambda.runtime.Context;
-import com.fasterxml.jackson.databind.JsonMappingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.core.type.TypeReference;
+import com.amazonaws.services.lambda.runtime.tests.annotations.Event;
 
-import java.util.HashMap;
 import java.util.Map;
-import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.stream.Stream;
 
 import com.amazonaws.xray.AWSXRay;
 import com.amazonaws.xray.AWSXRayRecorderBuilder;
@@ -29,40 +20,14 @@ class InvokeTest {
       AWSXRay.setGlobalRecorder(builder.build());
   }
 
-  @Test
-  void invokeTest() {
+  @ParameterizedTest
+  @Event(value = "event.json", type = Map.class)
+  void invokeTest(Map<String, String> event) {
       AWSXRay.beginSegment("blank-java-test");
-      String path = "src/test/resources/event.json";
-      String eventString = loadJsonFile(path);
-
-      ObjectMapper mapper = new ObjectMapper();
-      TypeReference<HashMap<String, String>> typeRef = new TypeReference<HashMap<String, String>>() {};
-      Map<String, String> event = new HashMap<>();
-      try {
-          event = mapper.readValue(eventString, typeRef);
-      } catch (JsonMappingException e) {
-          e.printStackTrace();
-      } catch (JsonProcessingException e) {
-          e.printStackTrace();
-      }
       Context context = new TestContext();
       Handler handler = new Handler();
       String result = handler.handleRequest(event, context);
-      assertTrue(result.contains("200 OK"));
+      assertTrue(result.contains("Total code size for your account"));
       AWSXRay.endSegment();
-  }
-
-  private static String loadJsonFile(String path)
-  {
-      StringBuilder stringBuilder = new StringBuilder();
-      try (Stream<String> stream = Files.lines( Paths.get(path), StandardCharsets.UTF_8))
-      {
-          stream.forEach(s -> stringBuilder.append(s));
-      }
-      catch (IOException e)
-      {
-          e.printStackTrace();
-      }
-      return stringBuilder.toString();
   }
 }
