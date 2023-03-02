6161f7e0a060990ccd0cb02fee93f666d4a1c92d
Alexander Yu <yualexan@amazon.com>
2023-03-02 13:06:34 -0800

    Update logging mechanism in blank-java
    - Instead of SLF4J logger, use Context's logger (removes SLF4J dependency)
    - However, during unit testing, still requires SLF4J. Tests still show this logging option.
    - Verified that Maven/Gradle builds don't generate SLF4J warnings.

--- sample-apps/blank-java/src/main/java/example/Handler.java
+++ sample-apps/blank-java/src/main/java/example/Handler.java
@@ -1,12 +1,10 @@
 package example;
 
 import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 
 import java.util.Map;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import software.amazon.awssdk.services.lambda.LambdaClient;
 import software.amazon.awssdk.services.lambda.model.GetAccountSettingsResponse;
@@ -15,14 +13,14 @@ import software.amazon.awssdk.services.lambda.model.LambdaException;
 // Handler value: example.Handler
 public class Handler implements RequestHandler<Map<String,String>, String> {
 
-    private static final Logger logger = LoggerFactory.getLogger(Handler.class);
     private static final LambdaClient lambdaClient = LambdaClient.builder().build();
 
     @Override
     public String handleRequest(Map<String,String> event, Context context) {
 
-        logger.info("ENVIRONMENT VARIABLES: " + System.getenv());
-        logger.info("EVENT: " + event);
+        LambdaLogger logger = context.getLogger();
+        logger.log("ENVIRONMENT VARIABLES: " + System.getenv());
+        logger.log("EVENT: " + event);
 
         GetAccountSettingsResponse response = null;
         try {
