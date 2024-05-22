41e1510f96dc7d52390d05e2e45b8044c6481beb
Ben Blanton <benblant@amazon.com>
2024-05-22 22:33:51 +0000

    JavaScript SDK v3 updates and deprecations

--- sample-apps/blank-nodejs/function/index.js
+++ sample-apps/blank-nodejs/function/index.js
@@ -1,26 +1,27 @@
-const AWSXRay = require('aws-xray-sdk-core')
-const AWS = AWSXRay.captureAWS(require('aws-sdk'))
+const AWSXRay = require('aws-xray-sdk-core');
+const { LambdaClient, GetAccountSettingsCommand } = require('@aws-sdk/client-lambda');
 
 // Create client outside of handler to reuse
-const lambda = new AWS.Lambda()
+const lambda = AWSXRay.captureAWSv3Client(new LambdaClient());
 
 // Handler
 exports.handler = async function(event, context) {
-  event.Records.forEach(record => {
-    console.log(record.body)
-  })
-  console.log('## ENVIRONMENT VARIABLES: ' + serialize(process.env))
-  console.log('## CONTEXT: ' + serialize(context))
-  console.log('## EVENT: ' + serialize(event))
-  
-  return getAccountSettings()
-}
+    event.Records.forEach(record => {
+        console.log(record.body);
+    });
+
+    console.log('## ENVIRONMENT VARIABLES: ' + serialize(process.env));
+    console.log('## CONTEXT: ' + serialize(context));
+    console.log('## EVENT: ' + serialize(event));
+
+    return getAccountSettings();
+};
 
 // Use SDK client
-var getAccountSettings = function(){
-  return lambda.getAccountSettings().promise()
-}
+var getAccountSettings = function() {
+    return lambda.send(new GetAccountSettingsCommand());
+};
 
 var serialize = function(object) {
-  return JSON.stringify(object, null, 2)
-}
+    return JSON.stringify(object, null, 2);
+};
